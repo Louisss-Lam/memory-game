810f8cbd5e60c78a00af64e5176fed40c68a84a4
Louisss-Lam <louislam617@gmail.com>
2022-12-13 17:06:59 +0000

    feat: add radio box

--- src/components/RadioBox/RadioBox.jsx
+++ src/components/RadioBox/RadioBox.jsx
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types';
 
 import styles from './RadioBox.module.css';
 
-const RadioBox = () => {
-    return <p>RadioBox</p>;
+const RadioBox = ({ name, selectedItem, onChange }) => {
+    const isChecked = name === selectedItem;
+
+    return (
+        <div className={`${styles.radioBox}`}>
+            <input type="radio" name={name} id={name} value={name} checked={isChecked} onChange={onChange} />
+            <label htmlFor={name}>{name}</label>
+        </div>
+    );
 };
 
 export default RadioBox;
+
+RadioBox.propTypes = {
+    name: PropTypes.string.isRequired,
+    selectedItem: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+}
