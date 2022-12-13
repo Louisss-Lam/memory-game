345e5eea701ea3bbdb8f98d90eb33ec7a105fb75
Louisss-Lam <louislam617@gmail.com>
2022-12-13 15:15:03 +0000

    add custom hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Background from './components/Background';
+import Settings from './components/Settings/Settings';
 
-const BASE_URL = 'https://api.pexels.com/v1/search?query=nature';
+import useGetImages from './hooks/useGetImages';
+
 function App() {
+    const images = useGetImages();
 
-    fetch(BASE_URL, {
-        headers:{
-            Authorization: process.env.REACT_APP_AUTH_KEY,
-        },
-    });
+    console.log(images);
     return (
         <>
             <Background />
             <h1>Memory Game</h1>
+            <Settings />
         </>
     );
 }
--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,5 +1,39 @@
-const BASE_URL = 'https://api.pexels.com/v1/search';
+import { useEffect, useState } from "react";
 
-const useGetImages = () => {};
+const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
+
+const useGetImages = () => {
+    const [images, setImages] = useState([]);
+
+    const buildUrl = () => {
+        let url = new URL('https://api.pexels.com/v1/search');
+
+        url.search = new URLSearchParams({
+            query: 'nature',
+            orientation: 'square',
+            size: 'small',
+            per_page: 2,
+            page: getRandomPage(),
+        });
+
+        return url;
+    };
+
+    const fetchPics = () => {
+        fetch(buildUrl(), {
+            headers:{
+                Authorization: process.env.REACT_APP_AUTH_KEY,
+            },
+        })
+        .then(data => data.json())
+        .then(data => setImages(data.photots));
+    }
+
+    useEffect(() => {
+        fetchPics();
+    }, []);
+
+    return images;
+};
 
 export default useGetImages;
